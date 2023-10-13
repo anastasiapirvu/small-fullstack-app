406c616c48f3243e02b384b2338db137d9c746e8
Anastasia Pirvu <anastasiapirvu@Anastasias-MacBook-Pro.local>
2023-10-13 15:32:01 +0200

    Button element render

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
 
-export default function Home({ allPostsData }) {
+
+export default function Home() {
+  const offerings = ['Toys', 'Tuna', 'Comfy bed'];
+  const [count, setCount] = useState([
+    { id: 1, name: "" },
+    { id: 2, name: "" },
+    { id: 3, name: "" },
+  ]);
+
+  function handleClick() {
+    setCount (count + 1);
+  }
+
   return (
     <Layout home>
       <Head>
@@ -16,29 +25,18 @@ export default function Home({ allPostsData }) {
         <p>🐾 Meet Furry Queen, the reigning monarch of the purr-fectly charming kingdom known as our living room. With a regal air that rivals even the stuffiest of aristocats, Furry Queen rules her domain with an iron paw, or perhaps more accurately, a velvety paw covered in... tuna juice? Yes, her majesty's demands are as quirky as they are endless. From demanding chin scratches at 3 AM to executing daring leaps onto the kitchen counter to inspect her treats, Furry Queen's antics keep us humans on our toes—and slightly sleep-deprived. So, prepare to be amused and utterly charmed by the one and only Furry Queen, the feline monarch of mayhem and mirth!🐾👑</p>
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <ul>
+        {offerings.map((offering) => (
+          <li key={offering}>{offering}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Give offering{offerings.map((offering) => (
+          <li key={offering}>{offering}</li>
+        ))}</button>
+
+      
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
